b162ecc8acb50a95ffed2b3c7898bea21058d372
Valiage <valiage@list.ru>
2016-02-23 18:28:34 +0200

    New changelist

---
+++ HomeWork's/src/Home_02_01_Min_of_3.java
@@ -0,0 +1,19 @@
+/**
+ * Найти минимальное из 3х чисел, причем, если минимальных несколько вывести все такие числа
+ */
+public class Home_02_01_Min_of_3 {
+
+    public static void main(String[] args) {
+
+        int a = 3;
+        int b = 5;
+        int c = 3;
+        int result;
+
+       // result = Math.min(int a, int b, int c);
+       // System.out.println(result);
+
+
+
+    }
+}
---
+++ HomeWork's/src/Home_02_04_Nearest.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Valiage on 19.02.2016.
+ */
+public class Home_02_04_Nearest {
+
+
+}
